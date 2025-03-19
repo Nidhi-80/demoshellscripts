1440066034480ce6e950e181c38ccc1d9cb81b8e
Nidhi-80 <nidhiece2014@gmail.com>
2025-03-19 15:24:20 +0000

    added dev file

---
+++ hello_chatur.sh
@@ -0,0 +1,22 @@
+#!/bin/bas
+#
+echo "chatur:aadarniya sabhpati mahodaya"
+
+echo "chatur:aaj hum chamtkar karenge"
+
+echo "rancho:ratta marega to aisa hi hoga"
+
+echo "chatur: thuchuk thuchuk"
+
+echo "raju: itna hi aata hai tujhe to ek folder bana k dikha"
+
+mkdir -p rancho
+
+echo "rancho; ye le, bana diya, ro mat ab"
+
+echo "farhan: virus ne beizzati kar di"
+
+echo  "farhan: abba nahi manenege" > farhanitrate.txt
+
+
+
